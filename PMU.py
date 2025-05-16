327e63cd07e412a3e802521770b4aade0eb522e9
Ramalakshmi Somanchi <rsomanchi@tns.org>
2025-05-16 12:06:41 +0530

    PMU.py

--- PMU.py
+++ PMU.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.exc import IntegrityError
 import pandas as pd
 from datetime import date
-import plotly.express as px
 import os
 
 # Set Streamlit page config (must be first)
