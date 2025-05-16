c5cb81e0f206582f1eb9f04f0ce7deaff62deeca
Ramalakshmi Somanchi <rsomanchi@tns.org>
2025-05-16 16:19:45 +0530

    PMU.py

--- PMU.py
+++ PMU.py
@@ -26,7 +26,7 @@ st.markdown("""
         }
 
         section[data-testid="stSidebar"] > div:first-child {
-            background-image: url("https://raw.githubusercontent.com/LakshmiSomanchi/PMU-/refs/heads/main/sidebar.jpg");
+            background-image: url("https://raw.githubusercontent.com/LakshmiSomanchi/PMU-/refs/heads/main/sidepm.jpg");
             background-size: cover;
             background-repeat: no-repeat;
             background-position: center;
