8ee08c6add0ae5eba2a2b91c4041f344e2a8eb86
Ramalakshmi Somanchi <rsomanchi@tns.org>
2025-05-26 17:20:37 +0530

    PMU.py

--- PMU.py
+++ PMU.py
@@ -284,7 +284,7 @@ def dashboard(user):
 
     for tab in dashboard_tabs:
         with tab:
-            st.subheader(f"📊 Progress in {tab}")
+            st.subheader(f"📊Progress")
             # Here you can add specific content for each dashboard
             # For example, you can display progress for each section
             st.write("This is where you can display progress and other metrics.")
