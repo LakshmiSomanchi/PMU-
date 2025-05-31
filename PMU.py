175774efc3bda2d673874bc9cdaafc5400ae9464
Ramalakshmi Somanchi <rsomanchi@tns.org>
2025-05-31 16:39:10 +0530

    PMU.py

--- PMU.py
+++ PMU.py
@@ -592,8 +592,7 @@ def heritage_dashboard():
     col3.metric("📈 Impact Index", "84.2")
 
     st.markdown("---")
-    
-    <div style="min-height:400px" 
+    <div style="min-height:300px" 
     id="datawrapper-vis-01h0U">
     <script type="text/javascript" defer src="https://datawrapper.dwcdn.net/01h0U/embed.js" charset="utf-8" data-target="#datawrapper-vis-01h0U">
     </script><noscript><img src="https://datawrapper.dwcdn.net/01h0U/full.png" alt="" />
